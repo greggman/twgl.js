e0f326d37a6f8f3475efd14b7b2c4fa3af5da317
Gregg Tavares <github@greggman.com>
2015-04-01 03:25:58 +0900

    use for in instead of Object.keys for each in setAttributes and setUniforms
    I prefer Object.keys for each but it's slower because it has to generate an
    array. Maybe that will be fixed in ES5 or ES6 some how?
    
    In any case for in usually requires checking hasOwnProperty but since
    we're already checking the setters it's unlikely to ever be an issue

--- src/twgl.js
+++ src/twgl.js
@@ -465,12 +465,12 @@ define([], function () {
    */
   function setUniforms(setters, values) {
     setters = setters.uniformSetters || setters;
-    Object.keys(values).forEach(function(name) {
+    for (var name in values) {
       var setter = setters[name];
       if (setter) {
         setter(values[name]);
       }
-    });
+    }
   }
 
   /**
@@ -562,12 +562,12 @@ define([], function () {
    * @deprecated use {@link module:twgl.setBuffersAndAttributes}
    */
   function setAttributes(setters, buffers) {
-    Object.keys(buffers).forEach(function(name) {
+    for (var name in buffers) {
       var setter = setters[name];
       if (setter) {
         setter(buffers[name]);
       }
-    });
+    }
   }
 
   /**
