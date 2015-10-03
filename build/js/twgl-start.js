825f05d82665d60373c85933dbbb5dd23408ca10
Gregg Tavares <github@greggman.com>
2015-10-04 00:25:57 +0200

    adjust twgl-start for commonjs

--- build/js/twgl-start.js
+++ build/js/twgl-start.js
@@ -1,6 +1,8 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
+    } if (typeof exports !== 'undefined') {
+        exports = factory();
     } else {
         root.twgl = factory();
     }
