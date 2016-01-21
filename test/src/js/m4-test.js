af4ec6964854e05d661fd9d19bf56f640721e1a4
Gregg Tavares <github@greggman.com>
2016-01-22 05:06:58 +0800

    add more tests

--- test/src/js/m4-test.js
+++ test/src/js/m4-test.js
@@ -105,6 +105,9 @@ describe('m4', function() {
     }, expected);
   });
 
+// TODO inverse
+// TODO multiply
+
   it('should set translation', function() {
     var expected = [
       0,  1,  2,  3,
@@ -237,7 +240,89 @@ describe('m4', function() {
     }, expected);
   });
 
-  it('should making scaling', function() {
+// TODO lookAt
+
+  it('should make translation matrix', function() {
+    var expected = [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      2, 3, 4, 1,
+    ];
+    testM4WithAndWithoutDest(function(dst) {
+      return m4.translation([2, 3, 4], dst);
+    }, expected);
+  });
+
+  it('should translate', function() {
+    var expected = [
+      0,  1,  2,  3,
+      4,  5,  6,  7,
+      8,  9, 10, 11,
+     12 + 0 * 2 + 4 * 3 + 8 * 4,
+     13 + 1 * 2 + 5 * 3 + 9 * 4,
+     14 + 2 * 2 + 6 * 3 + 10 * 4,
+     15 + 3 * 2 + 7 * 3 + 11 * 4,
+    ];
+    testM4WithAndWithoutDest(function(dst) {
+      return m4.translate(m, [2, 3, 4], dst);
+    }, expected);
+  });
+
+  it('should make x rotation matrix', function() {
+    var angle = 1.23;
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+    var expected = [
+      1, 0, 0, 0,
+      0, c, s, 0,
+      0,-s, c, 0,
+      0, 0, 0, 1,
+    ];
+    testM4WithAndWithoutDest(function(dst) {
+      return m4.rotationX(angle, dst);
+    }, expected);
+  });
+
+// TODO rotate x
+
+  it('should make y rotation matrix', function() {
+    var angle = 1.23;
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+    var expected = [
+      c, 0,-s, 0,
+      0, 1, 0, 0,
+      s, 0, c, 0,
+      0, 0, 0, 1,
+    ];
+    testM4WithAndWithoutDest(function(dst) {
+      return m4.rotationY(angle, dst);
+    }, expected);
+  });
+
+// TODO rotate y
+
+  it('should make z rotation matrix', function() {
+    var angle = 1.23;
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+    var expected = [
+      c, s, 0, 0,
+     -s, c, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1,
+    ];
+    testM4WithAndWithoutDest(function(dst) {
+      return m4.rotationZ(angle, dst);
+    }, expected);
+  });
+
+// TODO rotate z
+// TODO axisRotation
+// TODO axisRotate
+
+  it('should make scaling matrix', function() {
     var expected = [
       2, 0, 0, 0,
       0, 3, 0, 0,
@@ -260,6 +345,10 @@ describe('m4', function() {
       return m4.scale(m, [2, 3, 4], dst);
     }, expected);
   });
+
+// TODO transformPoint
+// TODO transformDirectiojn
+// TODO transformNormal
 
 });
 
