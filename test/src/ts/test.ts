58eed76fd1f095d59d3ed07fd0a9f76b129bbf81
Sophie Kirschner <sophie@thehumangeo.com>
2019-01-14 14:00:36 +0200

    Create a TypeScript test ("npm run testts")
    "npm run test" will now run both the pre-existing "js" tests as well as the new "ts" test. Use "npm run testjs" to run only the earlier tests.
    
    Borrows a handful of lines from code in the examples/ directory. It doesn't need to do anything interesting, it just needs to successfully compile.
    
    The TS test is not very thorough at this point, but it's a very solid start imo.

---
+++ test/src/ts/test.ts
@@ -0,0 +1,41 @@
+// Ensure that TypeScript can successfully compile with twgl as a dependency.
+// You probably want to run `npm run buildts` before running this test.
+
+import * as twgl from "../../../dist/4.x/twgl-full";
+
+window.addEventListener("onload", function() {
+    const canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
+    const gl = <WebGLRenderingContext> canvas.getContext("webgl");
+    twgl.setDefaults({attribPrefix: "a_"});
+    const programInfo: twgl.ProgramInfo = twgl.createProgramInfo(gl, [
+        "vs", "fs" // Placeholders
+    ]);
+    const identity: twgl.Mat4 = twgl.m4.identity();
+    const tex: WebGLTexture = twgl.createTexture(gl, {
+      minMag: gl.LINEAR,
+      wrap: gl.CLAMP_TO_EDGE,
+      src: [
+        255, 255, 255, 255,
+        192, 192, 192, 255,
+        192, 192, 192, 255,
+        255, 255, 255, 255,
+      ],
+    });
+    const shapes: twgl.BufferInfo[] = [
+      twgl.primitives.createCubeBufferInfo(gl, 2),
+      twgl.primitives.createSphereBufferInfo(gl, 1, 24, 12),
+      twgl.primitives.createPlaneBufferInfo(gl, 2, 2),
+      twgl.primitives.createTruncatedConeBufferInfo(gl, 1, 0, 2, 24, 1),
+      twgl.primitives.createCresentBufferInfo(gl, 1, 1, 0.5, 0.1, 24),
+      twgl.primitives.createCylinderBufferInfo(gl, 1, 2, 24, 2),
+      twgl.primitives.createDiscBufferInfo(gl, 1, 24),
+      twgl.primitives.createTorusBufferInfo(gl, 1, 0.4, 24, 12),
+    ];
+    
+    function render(time: number) {
+        twgl.resizeCanvasToDisplaySize(canvas);
+        requestAnimationFrame(render);
+    }
+    
+    requestAnimationFrame(render);
+});
