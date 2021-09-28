9850f58b31f82f71727551bf900619d7b9011acd
Gregg Tavares <gman@chromium.org>
2021-09-28 09:47:57 -0700

    typo

--- src/programs.js
+++ src/programs.js
@@ -1427,7 +1427,7 @@ function setUniformBlock(gl, programInfo, uniformBlockInfo) {
  *   You can also specify partial paths
  *
  *     twgl.setBlockUniforms(someBlockInfo, {
- *       'lights[1]: { intensity: 5.0, color: [1, 0, 0, 1] },
+ *       'lights[1]': { intensity: 5.0, color: [1, 0, 0, 1] },
  *     });
  *
  *   But you can not specify leaf array indices.
@@ -1611,7 +1611,7 @@ function setUniformTree(tree, values) {
  *   You can also specify partial paths
  *
  *     twgl.setUniforms(programInfo, {
- *       'lights[1]: { intensity: 5.0, color: [1, 0, 0, 1] },
+ *       'lights[1]': { intensity: 5.0, color: [1, 0, 0, 1] },
  *     });
  *
  *   But you can not specify leaf array indices
