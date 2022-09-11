509ea13c09d7e13c4e51a3ed13e14c3c6273dff9
Gregg Tavares <github@greggman.com>
2022-09-10 21:10:12 -0700

    update comment

--- src/attributes.js
+++ src/attributes.js
@@ -198,7 +198,9 @@ function makeTypedArray(array, name) {
  * @property {boolean} [normalize] whether or not to normalize the data. Default = false
  * @property {number} [offset] offset into buffer in bytes. Default = 0
  * @property {number} [stride] the stride in bytes per element. Default = 0
- * @property {number} [divisor] the divisor in instances. Default = 0.  Requires WebGL2 or the ANGLE_instanced_arrays extension.
+ * @property {number} [divisor] the divisor in instances. Default = 0.
+ *    Requires WebGL2 or the ANGLE_instanced_arrays extension.
+ *    and, if you using WebGL1 you must have called {@link module:twgl.addExtensionsToContext}
  * @property {WebGLBuffer} buffer the buffer that contains the data for this attribute
  * @property {number} [drawType] the draw type passed to gl.bufferData. Default = gl.STATIC_DRAW
  * @memberOf module:twgl
@@ -220,7 +222,9 @@ function makeTypedArray(array, name) {
  * @property {boolean} [normalize] normalize for `vertexAttribPointer`. Default is true if type is `Int8Array` or `Uint8Array` otherwise false.
  * @property {number} [stride] stride for `vertexAttribPointer`. Default = 0
  * @property {number} [offset] offset for `vertexAttribPointer`. Default = 0
- * @property {number} [divisor] divisor for `vertexAttribDivisor`. Default = 0.  Requires WebGL2 or the ANGLE_instanced_arrays extension.
+ * @property {number} [divisor] divisor for `vertexAttribDivisor`. Default = 0.
+ *     Requires WebGL2 or the ANGLE_instanced_arrays extension.
+ *     and, if you using WebGL1 you must have called {@link module:twgl.addExtensionsToContext}
  * @property {string} [attrib] name of attribute this array maps to. Defaults to same name as array prefixed by the default attribPrefix.
  * @property {string} [name] synonym for `attrib`.
  * @property {string} [attribName] synonym for `attrib`.
